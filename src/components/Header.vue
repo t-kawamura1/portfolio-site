4c9acbca4de42924608e03f1831e9ae708f21c4d
河村智之 <tkawamura@tkawamura.local>
2021-04-05 15:06:37 +0900

    [done] Header omponent, close #8

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,9 +1,15 @@
 <template>
-  <div class="header">
-    <header>
-      ヘッダー
-    </header>
-  </div>
+  <header class="header">
+    <h1 class="logo"><router-link to="/">T.K</router-link></h1>
+    <nav>
+      <ul class="global-nav">
+        <li class="nav-item"><a href="#" class="aaa">PORTFOLIO</a></li>
+        <li class="nav-item"><a href="#">SKILLS</a></li>
+        <li class="nav-item"><a href="#">STANCES</a></li>
+        <li class="nav-item"><a href="#">PROFILE</a></li>
+      </ul>
+    </nav>
+  </header>
 </template>
 
 <script>
@@ -14,6 +20,35 @@ export default {
 
 <style lang="scss" scoped>
 .header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  color: #636363;
+  padding: 0 30px;
   position: fixed;
+  z-index: 10;
+  background-color: rgba(255, 255, 255, 0.5);
+  width: 100%;
+  height: 54px;
+  a {
+    display: inline-block;
+    font-weight: 300;
+    color: #636363;
+    line-height: 40px;
+    height: 40px;
+    &:hover {
+      opacity: 0.5;
+    }
+  }
+  .global-nav {
+    display: flex;
+    justify-content: space-between;
+    .nav-item {
+      margin-right: 30px;
+      > a {
+        font-size: 1.1em;
+      }
+    }
+  }
 }
 </style>
