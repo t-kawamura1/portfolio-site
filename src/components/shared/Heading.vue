5bd021351979aba23482faf562772b10beffdbfb
河村智之 <tkawamura@tkawamura.local>
2021-04-09 16:53:11 +0900

    [add] heading decoration

--- src/components/shared/Heading.vue
+++ src/components/shared/Heading.vue
@@ -16,6 +16,14 @@ export default {
   font-size: 5rem;
   font-weight: 400;
   letter-spacing: 1px;
+  &::before, &::after {
+    content: '';
+    display: inline-block;
+    width: 100px;
+    height: 3px;
+    margin: 0 20px;
+    border-bottom: solid 3px;
+  }
 }
 
 @media (max-width: 768px) {
@@ -23,6 +31,12 @@ export default {
     margin-bottom: 40px;
     font-size: 3rem;
     font-weight: 300;
+    &::before, &::after {
+      width: 30px;
+      height: 2px;
+      margin: 0 10px;
+      border-bottom: solid 2px;
+    }
   }
 }
 </style>
